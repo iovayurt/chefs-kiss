7b8ae4629f5c59595c0741d574b217b3b622efe0
Tahir Ovayurt <iovayurt@gmail.com>
2021-09-16 14:41:57 +0300

    db

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,8 @@ class Booking < ApplicationRecord
   belongs_to :chef
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  def calculate_total_price
+    (end_time - start_time).to_i * chef.price
+  end
 end
